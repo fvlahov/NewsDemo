f5d2eb65568b8d408f2777dd42f0775443cf7628
frankovlahov <Franko.Vlahov@playhera.com>
2023-10-03 12:12:36 +0200

    [Fix] ISO 8601 date formatter

--- data/src/main/java/hr/vlahov/data/utils/DateUtils.kt
+++ data/src/main/java/hr/vlahov/data/utils/DateUtils.kt
@@ -5,7 +5,7 @@ import java.util.Date
 import java.util.Locale
 
 fun Long.toISO8601Date(): String? =
-    this.toDateFormat("yyyy-MM-ddTHH:mm:ss")
+    this.toDateFormat("yyyy-MM-dd")
 
 fun Long.toDateFormat(format: String, locale: Locale = Locale.US): String? =
     SimpleDateFormat(format, locale).format(Date(this))
